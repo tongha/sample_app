5b6d08084e8b8791a7be09f3e22e504453497204
tong <tong@clicker.com>
2011-03-28 14:16:02 -0700

    Done with sign in

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,7 +14,7 @@ describe UsersController do
       response.should have_selector("title", :content => "Sign up")
     end
   end
-
+  
   describe "GET 'show'" do
 
     before(:each) do
@@ -94,6 +94,11 @@ describe UsersController do
       it "should have a welcome message" do
         post :create, :user => @attr
         flash[:success].should =~ /welcome to the sample app/i
+      end
+      
+      it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
       end
     end
   end
